ab6f01b9fbf070e6a95b67ecb74236e6ab8f8707
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2020-03-21 18:14:00 -0300

    adaptando o html e css para a estrutura webpack-simple

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Projeto Gameboy Color - Pokemon Red</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  text-align: center;
+}
+</style>
